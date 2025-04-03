b2147a7d047e8e630577bee17a071c309bace794
ume <chi.nguyenthiyen.203@gmail.com>
2025-04-03 23:34:05 +0700

    Add fetching api lectures

---
+++ app/api/vocabulary/route.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { NextResponse } from "next/server";
+import { Readable } from "stream";
+
+const s3 = new S3Client({
+  endpoint: "https://s3.us-west-004.backblazeb2.com",
+  region: "us-west-004",
+  credentials: {
+    accessKeyId: process.env.B2_KEY_ID!,
+    secretAccessKey: process.env.B2_APP_KEY!,
+  },
+  forcePathStyle: true,
+});
+
+export async function GET() {
+  try {
+    const command = new GetObjectCommand({
+      Bucket: "nihongo-n5",
+      Key: "Vocabulary/Lecture1.json",
+    });
+
+    const result = await s3.send(command);
+    const stream = result.Body as Readable;
+    const chunks: any[] = [];
+
+    for await (const chunk of stream) {
+      chunks.push(chunk);
+    }
+
+    const json = JSON.parse(Buffer.concat(chunks).toString("utf-8"));
+    return NextResponse.json(json);
+  } catch (err) {
+    console.error(err);
+    return new NextResponse("Failed to fetch data", { status: 500 });
+  }
+}
