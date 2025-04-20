6595573037ee626466de0de094a5f4ab7f1bc69b
ume <chi.nguyenthiyen.203@gmail.com>
2025-04-20 22:38:34 +0700

    Update move between lectures

--- app/(dashboard)/lectures/[lectureId]/page.tsx
+++ app/(dashboard)/lectures/[lectureId]/page.tsx
@@ -5,25 +5,57 @@ import { useParams, useRouter } from "next/navigation";
 import { getLectureName } from "@/app/constants/lectures";
 import { Button } from "@/components/ui/button";
 import { lectureParts } from "@/app/constants/lectureParts";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function LecturePage() {
   const { lectureId } = useParams() as { lectureId: string };
   const name = getLectureName(lectureId);
   const router = useRouter();
 
-  const paths = [
-    { label: "All Lectures", href: "/lectures" },
-    { label: name },
-  ];
+  const match = lectureId.match(/^([a-zA-Z]+)(\d+)$/);
+  if (!match) throw new Error("Invalid lectureId format");
+
+  const text = match[1];
+  const num = parseInt(match[2]);
+
+  const paths = [{ label: "All Lectures", href: "/lectures" }, { label: name }];
 
   return (
-    <div className="py-6 px-4">
+    <div className="py-6 px-4 h-full">
       <Crumbs paths={paths} />
-      {/* <h2 className="text-xl font-bold mb-4">{name}</h2> */}
-      <h1 className="text-3xl font-bold my-4 text-primary">{name}</h1>
-      <div className="flex flex-wrap gap-2">{Object.entries(lectureParts).map(([partId, partName]) => (
-        <Button key={partId} className="" variant={"outline"} onClick={() => router.push(`${lectureId}/${partId}`)}>{partName}</Button>
-      ))}</div>
+      <div className="flex flex-row justify-center w-full h-full">
+        <div className="w-full lg:w-4/5 xl:w-3/5 2xl:w-1/2 flex flex-col">
+          <div>
+            <h1 className="text-3xl font-bold my-4 text-primary">{name}</h1>
+            <div className="flex flex-row gap-4">
+              {Object.entries(lectureParts).map(
+                ([partId, { label, icon: Icon }]) => (
+                  <div
+                    key={partId}
+                    className="w-full flex flex-col grid-cols-4 px-6 py-4 border-4 cursor-pointer rounded-md items-center text-base font-medium hover:font-semibold hover:border-muted-foreground"
+                    onClick={() => router.push(`${lectureId}/${partId}`)}
+                  >
+                    <div className="rounded-full border-2 p-4 mb-8">
+                      <Icon className="w-10 h-10 text-muted-foreground" />
+                    </div>
+                    {label}
+                  </div>
+                )
+              )}
+            </div>
+          </div>
+          <div className="flex-grow" />
+          <div className="flex justify-between mb-10">
+            <Button onClick={() => router.push(`${text+(num-1)}`)} disabled={num-1==0}>
+              <ChevronLeft />
+              Previous Lecture
+            </Button>
+            <Button onClick={() => router.push(`${text+(num+1)}`)} disabled={num+1==26}>
+              Next Lecture <ChevronRight />
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- app/constants/lectureParts.ts
+++ app/constants/lectureParts.ts
@@ -1,13 +1,20 @@
-export const lectureParts: Record<string, string> = {
-  "vocabulary": "Vocabulary",
-  "exercises": "Exercises",
-  // "reading_practices": "Reading Practices",
-  "grammar": "Grammar",
-  // "listening_practices": "Listening Practices",
-  "examination": "Examination",
-  // "references": "References",
-};
+import { Book, BookOpenText, LucideIcon, NotebookPen, School } from 'lucide-react';
+// export const lectureParts: Record<string, string> = {
+//   "vocabulary": "Vocabulary",
+//   "exercises": "Exercises",
+//   // "reading_practices": "Reading Practices",
+//   "grammar": "Grammar",
+//   // "listening_practices": "Listening Practices",
+//   "examination": "Examination",
+//   // "references": "References",
+// };
+export const lectureParts: Record<string, {label: string, icon: LucideIcon}> = {
+  "vocabulary": {label: "Vocabulary", icon: Book},
+  "grammar": {label: "Grammar", icon: BookOpenText},
+  "exercises": {label: "Exercises", icon: NotebookPen},
+  "examination": {label: "Examination", icon: School}
+}
 
 export function getLecturePart(id: string): string {
-  return lectureParts[id] || "Unknown Part";
+  return lectureParts[id]?.label || "Unknown Part";
 }
