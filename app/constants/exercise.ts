1b21de471a490060ae7fc9216cd5ce4e52dead51
ume <chi.nguyenthiyen.203@gmail.com>
2025-05-01 01:12:58 +0700

    Update Exercise Type

---
+++ app/constants/exercise.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// --- Interfaces (keep as before) ---
+interface Question {
+  id: string;
+  uniqueId?: string;
+  question: string;
+  question_kana?: string;
+  question_en?: string;
+  answer?: string[];
+  answer_kana?: string[];
+  passage?: string;
+  passage_kana?: string;
+  image?: string;
+  options?: string[];
+  options_kana?: string[];
+  sentence?: string;
+  // correctAnswer?: string;
+  words?: string[];
+}
+
+export interface ExercisePart {
+  id: string;
+  type: string;
+  title: string;
+  questions: Question[];
+  examples?: Question[];
+  images?: string[];
+}
