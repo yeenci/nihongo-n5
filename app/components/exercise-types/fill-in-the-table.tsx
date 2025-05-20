87b6cb6f9117d47f18d86b958b153185fbbe2cbb
ume <chi.nguyenthiyen.203@gmail.com>
2025-05-20 23:04:19 +0700

    Add FillInTheTable

--- app/components/exercise-types/fill-in-the-table.tsx
+++ app/components/exercise-types/fill-in-the-table.tsx
@@ -1,1 +1,20 @@
+import { Question } from "@/app/constants/exercise";
+
+interface FillInTheTableProps {
+  question: Question;
+  partId: string;
+  questionId: string;
+  value: string | string[] | undefined;
+  onChange: (
+    partId: string,
+    questionId: string,
+    value: string,
+    blankIndex?: number
+  ) => void;
+  isPartSubmitted: boolean;
+  result: (boolean | null) | (boolean | null)[] | undefined;
+  showKana: boolean;
+  getNumOfAnswers: (question: Question) => number;
+}
+
 export default function FillInTheTable() {}
