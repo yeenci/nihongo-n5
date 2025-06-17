9ebe18ddfd1b9e1d26b347335188700a064f6d87
ume <chi.nguyenthiyen.203@gmail.com>
2025-06-17 23:35:22 +0700

    update manage all post pages

--- app/(dashboard)/person/manage-posts/page.tsx
+++ app/(dashboard)/person/manage-posts/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function ManagePostsPage() {
   return (
-    <div>ManagePostsPage</div>
-  )
+    <div className="flex flex-row h-full justify-center w-full">
+      <div className="w-full lg:w-4/5 xl:w-3/5 2xl:w-1/2">
+        <h1 className="text-3xl font-bold my-4 text-primary">Your Posts</h1>
+      </div>
+    </div>
+  );
 }
