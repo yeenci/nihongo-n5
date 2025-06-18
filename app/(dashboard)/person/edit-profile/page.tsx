84cbab77c9d9566c82f6ff20eb58730cb9b4742c
ume <chi.nguyenthiyen.203@gmail.com>
2025-06-18 23:35:55 +0700

    update edit profile UI

--- app/(dashboard)/person/edit-profile/page.tsx
+++ app/(dashboard)/person/edit-profile/page.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function EditProfilePage() {
   return (
-    <div>page</div>
+      <div className="flex flex-row h-full justify-center w-full">
+        <div className="w-full lg:w-4/5 xl:w-3/5 2xl:w-1/2">
+          <h1 className="text-3xl font-bold my-4 text-primary">Edit Profile</h1>
+        </div>
+      </div>
   )
 }
