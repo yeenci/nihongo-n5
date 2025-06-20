bce014dbf01d5b4da8ffe1d9e3a0d6a391f7cdee
ume <chi.nguyenthiyen.203@gmail.com>
2025-06-20 23:37:56 +0700

    Edit only some parts

--- app/(dashboard)/person/edit-profile/page.tsx
+++ app/(dashboard)/person/edit-profile/page.tsx
@@ -5,6 +5,7 @@ export default function EditProfilePage() {
       <div className="flex flex-row h-full justify-center w-full">
         <div className="w-full lg:w-4/5 xl:w-3/5 2xl:w-1/2">
           <h1 className="text-3xl font-bold my-4 text-primary">Edit Profile</h1>
+          Edit only name, and password, address, ...
         </div>
       </div>
   )
