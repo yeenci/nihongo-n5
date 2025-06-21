5caa7cba13abce68d74ced4016cad73becbb8740
ume <chi.nguyenthiyen.203@gmail.com>
2025-06-21 22:02:44 +0700

    Change a little bit here

--- app/(dashboard)/person/manage-posts/page.tsx
+++ app/(dashboard)/person/manage-posts/page.tsx
@@ -5,7 +5,7 @@ export default function ManagePostsPage() {
     <div className="flex flex-row h-full justify-center w-full">
       <div className="w-full lg:w-4/5 xl:w-3/5 2xl:w-1/2">
         <h1 className="text-3xl font-bold my-4 text-primary">Your Posts</h1>
-        Posts here
+        All your posts are here!
       </div>
     </div>
   );
