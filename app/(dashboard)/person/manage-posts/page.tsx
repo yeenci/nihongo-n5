823761568d494ee9678643cfc76e1b7c42e8777e
ume <chi.nguyenthiyen.203@gmail.com>
2025-06-22 19:15:02 +0700

    fix ui for manage posts

--- app/(dashboard)/person/manage-posts/page.tsx
+++ app/(dashboard)/person/manage-posts/page.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@/components/ui/card";
 import React from "react";
 
 export default function ManagePostsPage() {
@@ -6,6 +7,7 @@ export default function ManagePostsPage() {
       <div className="w-full lg:w-4/5 xl:w-3/5 2xl:w-1/2">
         <h1 className="text-3xl font-bold my-4 text-primary">Your Posts</h1>
         All your posts are here!
+        <Card />
       </div>
     </div>
   );
