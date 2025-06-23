50d9350e93f66d7f7dfe448f1e9f4e9da01c3328
ume <chi.nguyenthiyen.203@gmail.com>
2025-06-23 23:28:08 +0700

    update call api for posts

--- app/(dashboard)/person/manage-posts/page.tsx
+++ app/(dashboard)/person/manage-posts/page.tsx
@@ -1,7 +1,12 @@
+import { useAuth } from "@/app/context/AuthContext";
 import { Card } from "@/components/ui/card";
+import { useFetchAllPosts } from "@/hooks/useFetchPosts";
 import React from "react";
 
 export default function ManagePostsPage() {
+  const { user } = useAuth();
+  const { posts, loading, error, refetchPosts } = useFetchAllPosts();
+
   return (
     <div className="flex flex-row h-full justify-center w-full">
       <div className="w-full lg:w-4/5 xl:w-3/5 2xl:w-1/2">
